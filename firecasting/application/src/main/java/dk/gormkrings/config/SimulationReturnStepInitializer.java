9ffbc160b57285b335b05a5115a91503e1c5e15d
Gorm2303 <gormdk333@gmail.com>
2025-12-20 22:27:08 +0100

    Config: add simulation.return.step (daily|monthly)

---
+++ firecasting/application/src/main/java/dk/gormkrings/config/SimulationReturnStepInitializer.java
@@ -0,0 +1,28 @@
+package dk.gormkrings.config;
+
+import dk.gormkrings.phase.callBased.SimulationCallPhase;
+import dk.gormkrings.simulation.ReturnStep;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SimulationReturnStepInitializer {
+
+    private final String configuredStep;
+
+    public SimulationReturnStepInitializer(
+            @Value("${simulation.return.step:daily}") String configuredStep
+    ) {
+        this.configuredStep = configuredStep;
+    }
+
+    @PostConstruct
+    public void init() {
+        ReturnStep step = ReturnStep.fromProperty(configuredStep);
+        SimulationCallPhase.configureReturnStep(step);
+        log.info("Simulation return step configured: {} (dt={})", step, step.toDt());
+    }
+}
